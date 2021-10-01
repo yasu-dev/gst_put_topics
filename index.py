ab36a76a9a4c569fb6a58e589a9b615859e46422
yasu-dev <71421346+yasu-dev@users.noreply.github.com>
2021-10-01 15:14:17 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,39 @@
+import sys
+import logging
+import boto3
 import json
-import datetime
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'Topic'
+table = dynamodb.Table(table_name)
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+
+    logger.info('PUT topics')
+
+    body = json.loads(event['body'])
+
+    response = table.update_item(
+        Key={
+            'topicId': int(event['pathParameters']['topicID'])
+        },
+        UpdateExpression="set topicTitle = :val1, topicBody = :val2, topicImageUrl = :val3, publish = :val4",
+        ExpressionAttributeValues={
+            ':val1': body['topicTitle'],
+            ':val2': body['topicBody'],
+            ':val3': body['topicImageUrl'],
+            ':val4': body['publish']
+        }
+    )
+
+    return {
+            'isBase64Encoded': False,
+            'statusCode': 200,
+            'headers': {
+              'Content-Type': 'application/json'
+            },
+            'body': json.dumps(body)
+            }
